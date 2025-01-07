3ad36a6881ff4f6a210efb7a7d91910b78425a5b
jeon hyebin <hyebin.jeon@g-mintech.co.kr>
2025-01-07 10:55:33 +0900

    - UcGraph 추가

---
+++ DischargerV2/UserControls/UcGraph.xaml.cs
@@ -0,0 +1,44 @@
+﻿using MExpress.Mex;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TreeView;
+
+namespace DischargerV2.UserControls
+{
+    /// <summary>
+    /// UcGraph.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class UcGraph : UserControl
+    {
+        public UcGraph()
+        {
+            InitializeComponent();
+
+            Loaded += UcGraph_Loaded;
+        }
+
+        private void UcGraph_Loaded(object sender, RoutedEventArgs e)
+        {
+            Rect rect = new Rect(0, 0, 0, 0);
+
+            double width = xGraphCanvas.ActualWidth;
+            double height = xGraphCanvas.ActualHeight;
+
+            rect.Height = 0.2;
+            rect.Width = 1 / Math.Ceiling(width / (height / 5));
+
+            xGraphVisualBrush.Viewport = rect;
+        }
+    }
+}
