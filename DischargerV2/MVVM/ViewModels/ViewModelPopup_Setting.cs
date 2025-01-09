3e049f3bc6a576b8dd472fed8ca6f2bd57816c04
jeon hyebin <hyebin.jeon@g-mintech.co.kr>
2025-01-09 11:53:48 +0900

    - 커밋 누락

---
+++ DischargerV2/MVVM/ViewModels/ViewModelPopup_Setting.cs
@@ -0,0 +1,17 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Sqlite.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DischargerV2.MVVM.ViewModels
+{
+    public class ViewModelPopup_Setting : BindableBase
+    {
+
+    }
+}
