e78cd3a2ef341b9c4c09d80a7d08776be3ec200e
jeon hyebin <hyebin.jeon@g-mintech.co.kr>
2025-06-24 11:50:27 +0900

    - 커밋 누락

---
+++ DischargerV2/Communication/ServerSqlite/ServerProcessor.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
+
+namespace Sqlite.Server
+{
+    public static class SqliteUserInfo
+    {
+        private static readonly string ClassName = "dbo.MST_USER_INFO";
+
+        private static string ConnectionString = "Data Source=127.0.0.1,1433;Initial Catalog=MINDIMS;User ID=sa;Password=mintech1234;";
+
+        public static void UpdateConnectionString(string serverIp, string serverPort, string databaseName)
+        {
+            ConnectionString = $@"Data Source={serverIp},{serverPort};Initial Catalog={databaseName};User ID=sa;Password=mintech1234;";
+        }
+
+        public static TABLE_MST_USER_INFO FindUserInfo(string userId, string password)
+        {
+            var userInfoList = GetData();
+
+            return userInfoList.Find(x => x.USER_ID == userId && x.PASSWD == password);
+        }
+
+        public static List<TABLE_MST_USER_INFO> GetData()
+        {
+            List<TABLE_MST_USER_INFO> table = new List<TABLE_MST_USER_INFO>();
+
+            using (SqliteConnection connection = new SqliteConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string query = @"SELECT * FROM " + ClassName;
+
+                using (SqliteCommand command = new SqliteCommand(query, connection))
+                {
+                    SqliteDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        TABLE_MST_USER_INFO tableMstUserInfo = new TABLE_MST_USER_INFO
+                        {
+                            USER_ID = reader["USER_ID"].ToString(),
+                            USER_NM = reader["USER_NM"].ToString(),
+                            PASSWD = reader["PASSWD"].ToString(),
+                            ADMIN_GROUP = reader["ADMIN_GROUP"].ToString(),
+                            USE_YN = reader["USE_YN"].ToString(),
+                        };
+
+                        table.Add(tableMstUserInfo);
+                    }
+                }
+            }
+
+            return table;
+        }
+    }
+}
---
+++ DischargerV2/Communication/ServerSqlite/ServerStructure.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sqlite.Server
+{
+    public class TABLE_MST_USER_INFO
+    {
+        public string USER_ID { get; set; }
+        public string USER_NM { get; set; }
+        public string PASSWD { get; set; }
+        public string ADMIN_GROUP { get; set; }
+        public string USE_YN { get; set; }
+    }
+
+    public class TABLE_SYS_STS_SDC
+    {
+
+    }
+}
